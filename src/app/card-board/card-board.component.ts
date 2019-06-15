8cc789f4d01d2d77608adf1f335def34cb269a21
Abel <esung1129@gmail.com>
2019-06-15 22:09:54 +0900

    :sparkles: drag and drop 핵심기능 추가

--- src/app/card-board/card-board.component.ts
+++ src/app/card-board/card-board.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: "app-card-board",
@@ -8,7 +13,24 @@ import { Component, OnInit } from "@angular/core";
 export class CardBoardComponent implements OnInit {
   constructor() {}
 
+  ngOnInit() {}
+
   numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  ngOnInit() {}
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
--- src/app/card-hole-board/card-hole/card-hole.component.ts
+++ src/app/card-hole-board/card-hole/card-hole.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: "app-card-hole",
@@ -7,8 +12,26 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class CardHoleComponent implements OnInit {
   @Input() holeNumber: number;
+  hole = [];
 
   constructor() {}
 
   ngOnInit() {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
