3245923e23fd0164dbeadbcbdb5280a576075a2b
abelko <esung1129@gmail.com>
2019-06-27 00:32:27 +0900

    :art: 3번 성공시, 다음단계로 이동

--- src/app/level3/level3.component.ts
+++ src/app/level3/level3.component.ts
@@ -37,30 +37,24 @@ export class Level3Component implements OnInit {
 
   checkNumberList() {
     const result = this.numberList.every(v => !v.isInput)
-    console.log(this.gameStatus.count)
-
-    // 모두 true이고, 현재 단계 3번 이하일때 다시 고고
-    if (result && this.gameStatus.count < 2) {
+    if (result) {
       this.gameStatus.count++;
-      this._setGame();
-    }
-
-    // 1단계 3번 클리어 한다면,
-    // 다음 스테이지
-    if (result && this.gameStatus.count > 2) {
-      this.gameStatus.level++;
-      // 1      // 2     // 3
-      //1, 30 // 31, 60 // 61, 90//
-      // this.randomNumbers = this._getRandomNumbers(31, 60);
-      const { level } = this.gameStatus;
-      this.randomNumbers = this._getRandomNumbers(1 + 30 * (level - 1), 30 * level);
-      this.numberList = this.numberList.map(({ value, isInput }) => {
-        return { value: value + 30, isInput }
-      })
-        .map(({ value, isInput }) => {
-          if (this.randomNumbers.includes(value)) isInput = true;
-          return { value, isInput }
+      if (this.gameStatus.count < 3) {
+        this._setGame();
+      }
+      if (this.gameStatus.count >= 3) {
+        this.gameStatus.level++;
+        this.gameStatus.count = 0;
+        const { level } = this.gameStatus;
+        this.randomNumbers = this._getRandomNumbers(1 + 30 * (level - 1), 30 * level);
+        this.numberList = this.numberList.map(({ value, isInput }) => {
+          return { value: value + 30, isInput }
         })
+          .map(({ value, isInput }) => {
+            if (this.randomNumbers.includes(value)) isInput = true;
+            return { value, isInput }
+          })
+      }
     }
   }
 
@@ -73,9 +67,10 @@ export class Level3Component implements OnInit {
   }
 
   _setGame() {
-    this.randomNumbers = this._getRandomNumbers(1, 30);
+    const { level } = this.gameStatus;
+    this.randomNumbers = this._getRandomNumbers(1 + 30 * (level - 1), 30 * level);
     this.numberList = new Array(30).fill(null).map((_, i) => {
-      return { value: i + 1, isInput: false }
+      return { value: i + 1 + ((level - 1) * 30), isInput: false }
     })
       .map(({ value, isInput }) => {
         if (this.randomNumbers.includes(value)) isInput = true;
